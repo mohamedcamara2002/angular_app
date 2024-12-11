2df6d3956b07c5288bf189d6916b4d5810b5ce11
makhmadane96 <makhmadane.lo@jmadsolutions.sn>
2024-12-11 21:32:56 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { ProfilComponent } from './profil/profil.component';
+import { FormsModule } from '@angular/forms';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ProfilComponent,
+    UtilisateurComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
